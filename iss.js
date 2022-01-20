5c5cff16b78820ef2efef1a65a70fc6f504a63d9
Albert Ho <albertcyi.ho@gmail.com>
2022-01-20 14:43:31 -0800

    Added fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -10,22 +10,37 @@ const request = require("request");
 
 const fetchMyIP = function (callback) {
   // use request to fetch IP address from JSON API
-  request(
-    "https://api.ipify.org/?format=json",
-    function (error, response, body) {
-      if (error) return callback(error, null);
+  request("https://api.ipify.org/?format=json", (error, response, body) => {
+    if (error) return callback(error, null);
 
-      if (response.statusCode !== 200) {
-        return callback(
-          Error(`Status Code ${response.statusCode} when fetching IP: ${body}`),
-          null
-        );
-      }
-
-      const ip = JSON.parse(body).ip;
-      callback(null, ip);
+    if (response.statusCode !== 200) {
+      return callback(
+        Error(`Status Code ${response.statusCode} when fetching IP: ${body}`),
+        null
+      );
     }
-  );
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      return callback(
+        Error(
+          `Status Code ${response.statusCode} when fetching coordinates for IP: ${body}`
+        ),
+        null
+      );
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
